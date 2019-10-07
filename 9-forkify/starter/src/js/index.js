71c0425bc3f6ae11c0a00069f9076e2f0cc903c3
tony trinh <tony.w.trinh3@gmail.com>
2019-10-06 22:41:46 -0700

    search view

--- 9-forkify/starter/src/js/index.js
+++ 9-forkify/starter/src/js/index.js
@@ -1,4 +1,45 @@
-// Global app controller
-import num from './test';
+import Search from './models/Search';
 
-console.log(`i import ${num}`);
+
+//state is all data in one place in one object
+// global state of the app
+/*
+-search object
+-current recipe object
+-shopping list object
+-liked recipes
+*/
+
+const state = {};
+
+//you need async here bc you are using an await within the function itself bc of getResults()
+const controlSearch = async () =>{
+
+    //1. get query from view
+    const query = 'pizza';
+    
+    if(query){
+        //2. new search object and add to state
+        state.search = new Search(query)
+
+        //3. prepare UI for results
+
+        //4. search for recipes
+        //getResults() returns a promise so you need await to run it 
+        await state.search.getResults();
+
+        //5. render results on UI
+        console.log(state.search.result);
+
+
+    }
+}
+//e is event that is within callback function
+document.querySelector('.search').addEventListener('submit', e =>{
+    //you need this to prevent page from reloading every time you press button
+    e.preventDefault();
+    controlSearch();
+})
+
+
+
---
+++ 9-forkify/starter/src/js/models/Search.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+
+export default class Search {
+    //class is a es6 feature
+    //you make class in order to duplicate object 
+
+    //same as es5 function 
+    constructor(query){
+        this.query = query;
+    }
+
+    async getResults(){
+        // const proxy = '';
+        const key = `e864c36ed7da2286ddeccaddc0adcc7c`;
+        try{
+            //axios used in this case is same as fetch except axios can be used in all browsers unlike fetch and axios catch errors better 
+            const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${this.query}`);
+            //change from a const to this. allows you to save recipes into the object when you use it 
+            this.result = res.data.recipes;
+            
+        } catch (error){
+            alert(error);
+        }
+    
+    }
+    
+    
+}
