fb00c280c580e4fda8702fa9b00be92d8da45e1d
tony trinh <tony.w.trinh3@gmail.com>
2019-10-16 00:04:15 -0700

    listview

--- 9-forkify/starter/src/js/index.js
+++ 9-forkify/starter/src/js/index.js
@@ -166,6 +166,8 @@ elements.recipe.addEventListener('click', e =>{
         //Increase button is clicked
         state.recipe.updateServings('inc');
         recipeView.updateServingsIngredients(state.recipe);
+    } else if (e.target.matches('.recipe__btn--add, .recipe__btn--add *')){
+        controlList();
     }
     console.log(state.recipe);
 });
